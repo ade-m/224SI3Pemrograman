6399c59854ce2b1b8e4ce577218979329a108d00
ade-m <66313509+ade-m@users.noreply.github.com>
2024-09-03 23:07:24 +0700

    Mencari bilangan genap dan ganjil

---
+++ pertemuan3-4.py
@@ -0,0 +1,12 @@
+#Bilangan Ganjil Genap
+
+#inisialisasi variabel A
+A= int(input("A\t:\t"))
+
+#bilangan genap : bilang %2 ==0
+if (A%2)==0 :
+    print("A merupakan bilangan Genap")
+#if (A%2)!=0 :
+else :
+    print("A merupakan bilangan Ganjil")
+
